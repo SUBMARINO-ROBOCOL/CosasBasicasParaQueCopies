9e378a935294cae519e812ee65875015886a6414
3xDaniel <124632083+3xDaniel@users.noreply.github.com>
2023-06-10 10:20:14 -0500

    Update cam_boton.py
    Cambio de limites, detecta rojo pero es sensible
    
    Cambiar valores lower1, lower2, upper1, upper2 para mejor resultado
    
    Un entorno sin ruido y más azul quizás

--- ros2_camera_ws/src/cam_stream/cam_stream/lib/cam_boton.py
+++ ros2_camera_ws/src/cam_stream/cam_stream/lib/cam_boton.py
@@ -3,31 +3,35 @@ from PIL import Image
 import numpy as np
 
 
-def get_limits(color):
-
-    c = np.uint8([[color]])
-    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
-
-    lowerLimit = hsvC[0][0][0] - 10, 100, 100
-    upperLimit = hsvC[0][0][0] + 10, 255, 255
-
-    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
-    upperLimit = np.array(upperLimit, dtype=np.uint8)
-
-    return lowerLimit, upperLimit
+#def get_limits(color):
+#
+#    c = np.uint8([[color]])
+#    hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
+#
+#    lowerLimit = hsvC[0][0][0] - 10, 100, 100
+#    upperLimit = hsvC[0][0][0] + 10, 255, 255
+#
+#    lowerLimit = np.array(lowerLimit, dtype=np.uint8)
+#    upperLimit = np.array(upperLimit, dtype=np.uint8)
+#
+#    return lowerLimit, upperLimit
 
 
 def red_box(img):
 	bbox = (0,0,0,0)
-
-	color = [0,255,255]
 	
 	hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-		
-	lowerLimit, upperLimit = get_limits(color)
-		
-	mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
-		
+	
+	lower1 = np.array([0, 100, 20])
+	upper1 = np.array([10, 255, 255])
+	lower2 = np.array([160,100,20])
+	upper2 = np.array([179,255,255])
+
+	lower_mask = cv2.inRange(hsvImage, lower1, upper1)
+	upper_mask = cv2.inRange(hsvImage, lower2, upper2)
+
+	mask = lower_mask + upper_mask
+
 	mask_ = Image.fromarray(mask)
 	bbox = mask_.getbbox()
 	if bbox is not None:
