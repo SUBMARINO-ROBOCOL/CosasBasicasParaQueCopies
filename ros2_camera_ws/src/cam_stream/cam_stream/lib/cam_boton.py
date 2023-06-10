8afa2dbd5efdd3d88d2479249f88a6f7168cee53
3xDaniel <124632083+3xDaniel@users.noreply.github.com>
2023-06-10 10:08:07 -0500

    Update cam_boton.py
    Funciona pero raro, toca mantener presionado Q habiendo seleccionado el frame para dar video fluido, presionar Q de manera alternante muestra las imagenes frame por frame
    
    Está detectando amarillo, cambios para detectar rojo pendientes

--- ros2_camera_ws/src/cam_stream/cam_stream/lib/cam_boton.py
+++ ros2_camera_ws/src/cam_stream/cam_stream/lib/cam_boton.py
@@ -8,8 +8,8 @@ def get_limits(color):
     c = np.uint8([[color]])
     hsvC = cv2.cvtColor(c, cv2.COLOR_BGR2HSV)
 
-    lowerLimit = hsvC[0][0][0] + 160, -50, -50
-    upperLimit = hsvC[0][0][0] + 180, 200, 200
+    lowerLimit = hsvC[0][0][0] - 10, 100, 100
+    upperLimit = hsvC[0][0][0] + 10, 255, 255
 
     lowerLimit = np.array(lowerLimit, dtype=np.uint8)
     upperLimit = np.array(upperLimit, dtype=np.uint8)
@@ -18,25 +18,23 @@ def get_limits(color):
 
 
 def red_box(img):
-	cap = img
-	width = cap.get(3)
-	height = cap.get(4)
+	bbox = (0,0,0,0)
+
+	color = [0,255,255]
 	
-	color = [0,0,255]
-	lower_red = np.array([0, 50, 70])
-	uppper_red = np.array([9, 255, 255])
+	hsvImage = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+		
+	lowerLimit, upperLimit = get_limits(color)
+		
+	mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
+		
+	mask_ = Image.fromarray(mask)
+	bbox = mask_.getbbox()
+	if bbox is not None:
+		x1, y1, x2, y2 = bbox
+		img = cv2.rectangle(img, (x1,y1), (x2,y2), (50,50,250), 5)
 	
 	while True:
-		ret, frame = cap.read()
-		
-		hsvImage = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-		
-		lowerLimit, upperLimit = get_limits(color)
-		
-		mask = cv2.inRange(hsvImage, lowerLimit, upperLimit)
-		
-		mask_ = Image.fromarray(mask)
-		bbox = mask_.getbbox()
-		x1, y1, x2, y2 = bbox
-		frame = cv2.rectangle(frame, (x1,y1), (x2,y2), (50,50,250), 5)
-		cv2.imshow('frame', frame)
+		cv2.imshow ("frame", img)
+		if cv2.waitKey(1) & 0xFF == ord("q"):
+			break
