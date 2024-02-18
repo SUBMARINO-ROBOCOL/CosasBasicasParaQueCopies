09bc138b2e79189fd05d6fa6080f69943a1e9c8f
ElSergioOliveros <s.oliverosb@uniandes.edu.co>
2024-02-18 09:25:55 -0500

    more stuff

--- TestLatencia/build/aqui/prefix_override/sitecustomize.py
+++
@@ -1,3 +0,0 @@
-import sys
-sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/sarten-imusa-420/Documents/GitHub/TestSubRobocol/TestLatencia/install/aqui'
--- ros2_camera_ws/build/cam_stream/prefix_override/sitecustomize.py
+++ ros2_camera_ws/build/cam_stream/prefix_override/sitecustomize.py
@@ -1,3 +1,3 @@
 import sys
 sys.real_prefix = sys.prefix
-sys.prefix = sys.exec_prefix = '/home/sarten-imusa-420/Documents/GitHub/CosasBasicasParaQueCopies/ros2_camera_ws/install/cam_stream'
+sys.prefix = sys.exec_prefix = '/home/uwu/Documents/CosasBasicasParaQueCopies/ros2_camera_ws/install/cam_stream'
